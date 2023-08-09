06c1e0be57ee076af06d9bd3a4d77a3f360f5862
Kenya Gonzalez <kenya.gonzalez@solera.com>
2023-08-09 09:20:12 +0200

    Fnished section 5. Added more communication between parent/child components. Explored derived state (now commented because not useful).

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,15 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.expense.title);
-
-  const clickHandler = () => {
-    setTitle("updated!!!!");
-  };
-
-  return ( 
+  return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
-      <h2>{title}</h2>
+      <h2>{props.expense.title}</h2>
       <div className="expense_item__description">
         <div className="expense-item__price">${props.expense.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -2,17 +2,34 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./ExpenseList.css";
 import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const ExpenseList = (props) => {
-  
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  // let filterInfoText = "2019, 2021, 2022 & 2023";
+
+  // if (selectedYear === "2019") {f
+  //        filterInfoText = '2020, 2021, 2022 & 2023';
+  //      } else if (selectedYear === "2020") {
+  //       filterInfoText = '2019, 2021, 2022 & 2023';
+  //      } else if (selectedYear === "2021") {
+  //       filterInfoText = '2019, 2020, 2022 & 2023';
+  //      } else if (selectedYear === "2022") {
+  //       filterInfoText = '2019, 2020, 2021 & 2023';
+  //      } else {
+  //       filterInfoText = '2019, 2020, 2021 & 2022';
+  //      }
+
   const selectYearHandler = (selectedYear) => {
-    console.log(selectedYear);
+    setFilteredYear(selectedYear);
   };
-  
+
   return (
     <div>
-      <ExpensesFilter onSelectYear={selectYearHandler} />
       <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onSelectYear={selectYearHandler} />
+        {/* <p>Data for years {filterInfoText} is hidden </p> */}
         {props.expenseArray.map((expense) => (
           <ExpenseItem key={expense.id} expense={expense} />
         ))}
